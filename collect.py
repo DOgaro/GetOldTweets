61aae584c6578b4897e89b01a08e6f32a4141bfb
David Ogaro <38218784+DOgaro@users.noreply.github.com>
2020-05-11 12:23:44 +0300

    Update collect.py

--- collect.py
+++ collect.py
@@ -1,9 +1,9 @@
 import tweepy
 import csv
-consumer_key="gdIYUAOypp7dgFEMWz9wpuVC7"
-consumer_secret="wqzYx4jHngjlaXMN4qTbR9Vn3szoBT2sukPuUjKuZpoAd0meHX"
-access_token="806066029952729088-N1gWDFhxLGMgIIfBj47uUBJmeeMDNt6"
-access_secret="hGqcm517Gl4g5NkVcOijWlXK0duMjtwAj0jf3hRYzLP2D"
+consumer_key=""
+consumer_secret=""
+access_token=""
+access_secret=""
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
